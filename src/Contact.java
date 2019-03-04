2c7dce5d44e8c9c1d450b0e283302b67bf85f5a4
Varun Kumar <varun.thegreate.kumar@gmail.com>
2019-03-04 09:11:31 -0600

    get it done you idiot

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,42 @@
 public class Contact {
+    String firstName="",lastName="",address="";
+    int number;
+    public Contact(String firstName, String lastName, int number, String address) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.number=number;
+        this.address=address;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/RoliPokeDex.java
+++ src/RoliPokeDex.java
@@ -1,9 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class RoliPokeDex extends JFrame {
+    JTextField firstName=new JTextField();
+    JTextField lastName=new JTextField();
+    JTextField phoneNumber=new JTextField();
+    JTextField address=new JTextField();
+    JLabel firstNameLabel=new JLabel("First Name: ");
+    JLabel lastNameLabel=new JLabel("Last Name: ");
+    JLabel phoneNumberLabel=new JLabel("Phone Number: ");
+    JLabel addressLabel=new JLabel("Address: ");
+    Font font = new Font("Monotype Corsiva",Font.BOLD,24);
+
+    /*TODO: Change buttons (Save Changes and Delete contact and Save an New) change via setVisible().
+        TODO: Have a are you sure window open when someone clicks away from the window while editing, does not save changes. Get the text from the Contact object.
+        TODO: List of contact on the side should be scrollable when the list extends past the window
+        TODO: List of contact on the side is a JScrollPane, it automatically updates to scroll.
+        TODO: If an index is selected, change the buttons, seen in the first sentence.
+        TODO: Contact is what all the textfields are got from. Only when save changes is clicked, does the contact object update
+     */
+
     public RoliPokeDex() {
         super("K0904676");
-        setSize(600,800);
+        setVisible(true);
+        setLayout(null);
+        setSize(800,600);
+        firstNameLabel.setFont(font);
+        firstNameLabel.setBounds(5,20,150,30);
+        lastNameLabel.setBounds(5,60,150,30);
+        lastNameLabel.setFont(font);
+
+        add(firstNameLabel);
+        add(lastNameLabel);
 
     }
 }
